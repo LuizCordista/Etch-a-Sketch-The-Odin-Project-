ca927b214a48dfbee7867c03a2cccebc6c5a382a
luizcordista <luizcordista@gmail.com>
2024-04-12 15:22:33 -0300

    Adiciona botão para limpar a grid.

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ function newDiv(number) {
 
 
 function grid(number) {
-    container.innerHTML = '';
+    container.innerHTML = "";
     for(let i = 0; i < (number * number); i++) {
         container.appendChild(newDiv(number));
     }
-    const paints = document.querySelectorAll('.paint');
+    const paints = document.querySelectorAll(".paint");
 
     paints.forEach(paint => {
-        paint.addEventListener('mouseover', () => {
+        paint.addEventListener("mouseover", () => {
             if (colorCount == 5) {
                 colorCount = 0
             }
@@ -32,7 +32,7 @@ function grid(number) {
     })
 }
 
-const newGrid = document.querySelector('.newgrid');
+const newGrid = document.querySelector(".newgrid");
 
 newGrid.addEventListener("click", () => {
     let number = prompt("Quantos quadrados por lado? (max 100)");
@@ -40,6 +40,15 @@ newGrid.addEventListener("click", () => {
         number = prompt("Por favor escolha um número menor que 100.")
     }
     grid(number);
+})
+
+
+const clear = document.querySelector(".clear")
+clear.addEventListener("click", () => {
+    const paints = document.querySelectorAll(".paint");
+    paints.forEach(paint => {
+        paint.style.backgroundColor = "white"
+    })
 })
 
 
